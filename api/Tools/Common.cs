9924c47aebceb50f60f31097d9988a51e42ba343
rommelalansanchez <rommel.alan.sanchez@gmail.com>
2024-10-23 17:25:59 -0700

    Cambia plantilla de contacto a solo texto

--- api/Tools/Common.cs
+++ api/Tools/Common.cs
@@ -1,12 +1,11 @@
-﻿using System.IO;
-
-namespace api.Tools
+﻿namespace api.Tools
 {
     public class Common
     {
         public static string ObtenerPlantillaContacto(string nombre, string correo, string telefono, string asunto, string mensaje)
         {
-            string readText = File.ReadAllText("RespuestaContacto.html")
+            string readText = "Contacto desde la página web de Vínculos y Esperanza.\r\n<br />\r\n<strong>Datos de contacto</strong>\r\n<p>\r\n  <strong>Nombre:</strong> {{nombre}}\r\n  <br />\r\n  <strong>Asunto:</strong> {{asunto}}\r\n  <br />\r\n  <strong>Correo:</strong> {{correo}}\r\n  <br />\r\n  <strong>Teléfono:</strong> {{telefono}}\r\n  <br />\r\n  <strong>Mensaje:</strong> {{mensaje}}\r\n</p>\r\n"
+                //File.ReadAllText("RespuestaContacto.html")
                 .Replace("{{nombre}}", nombre)
                 .Replace("{{correo}}", correo)
                 .Replace("{{telefono}}", telefono)
